58119baf0990a64d8196bb572d192a9bb5783040
fullstackNINJA05 <prempanchal0501@gmail.com>
2026-04-09 19:42:31 +0530

    added 9th april folder to src

---
+++ src/April_8th_2026/Leetcode_Ques_8.java
@@ -0,0 +1,34 @@
+package April_8th_2026;
+import java.util.*;
+public class Leetcode_Ques_8 {
+    public static int myAtoi(String s) {
+        int n = s.length();
+        int i = 0;
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        int sign = 1;
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            if (s.charAt(i) == '-')
+                sign = -1;
+            i++;
+        }
+        int ans = 0;
+        while (i < n && Character.isDigit(s.charAt(i))) {
+            int dgt = s.charAt(i) - '0';
+            if (ans > (Integer.MAX_VALUE - dgt) / 10) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            ans = ans * 10 + dgt;
+            i++;
+        }
+
+        return ans * sign;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+
+        System.out.println(myAtoi(s));
+    }
+}
--- src/April_8th_2026/demofile.java
+++
@@ -1,9 +0,0 @@
-package April_8th_2026;
-
-public class demofile {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-    }
-}
